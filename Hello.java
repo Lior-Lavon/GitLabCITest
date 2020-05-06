571bc21e3b73fda15361421b01c2b7fdcb800a1d
Lior Lavon <>
2020-05-06 23:41:54 +0200

    new build 3

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("Hello World from GitLan Runner");
-        System.out.println("2");
+        System.out.println("3");
     }
 }
